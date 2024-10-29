b5ca2e7f1833f1a0da01ace09e541be9140911a1
Pedro Esteves <pedroesteves2803@gmail.com>
2024-10-29 01:39:42 -0300

    WIP: Alterar nome de tabela

--- app/Models/Home.php
+++ app/Models/Home.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Home extends Model
 {
-    protected $table = 'homes';
+    protected $table = 'home';
 
     protected $fillable = [
         'title',
