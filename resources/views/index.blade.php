d0f269f71ea79338c43a96454f7ea61fe8c736a0
Pedro Esteves <pedroesteves2803@gmail.com>
2024-11-03 18:07:50 -0300

    WIP: Criando css.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Quintal brinquedos</title>
+        <link rel="stylesheet" href="{{ asset('scss/main.css') }}">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Hind+Vadodara:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <h1>Teste</h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores iure, consequuntur repellendus dicta veniam laboriosam, voluptate dolore rem vitae inventore placeat maiores nam rerum. Incidunt dolorum vero ea non. Consequatur?</p>
+        </div>
+    </body>
+</html>
