195a7d9ecc0cf00adb77b7dc01d6cee099ffe97b
Pedro Esteves <pedroesteves2803@gmail.com>
2024-11-11 01:55:37 -0300

    WIP

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,76 +1,204 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Quintal brinquedos</title>
-        <link rel="stylesheet" href="{{ asset('css/main.css') }}">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
-    </head>
-    <body>
-        <header class="header">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quintal Brinquedos</title>
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hind+Vadodara:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header class="header">
+        <div class="container">
+
+            <div class="items">
+                <img class="logo" src="{{ asset('images/page/logo.png') }}" alt="Logo - Quintal Brinquedos">
+
+                <nav class="navigation">
+                    <ul class="menu">
+                        <li><a href="/">Sobre</a></li>
+                        <li><a href="/">Produtos</a></li>
+                        <li><a href="/">Lojas</a></li>
+                        <li><a href="/">Contato</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="home-wrapper">
             <div class="container">
+                <div class="home">
+                    <div class="home-content">
+                        <h1 class="home-year">CATÁLOGO | 2025</h1>
 
-                <div class="itens">
-                    <nav class="nav">
-                        <ul class="menu">
-                          <li><a href="/">Sobre</a></li>
-                          <li><a href="/">Produtos</a></li>
-                          <li><a href="/">Lojas</a></li>
-                          <li><a href="/">Contato</a></li>
-                        </ul>
-                    </nav>
+                        <h2 class="home-main-title">Os melhores brinquedos,</h2>
+                        <h2 class="home-subtitle">Estão aqui!</h2>
 
-                    <img class="logo" src="{{ asset('images/page/logo.png') }}" alt="Logo - Quintal brinquedos">
+                        <p class="home-description">Entregamos e Instalamos</p>
 
+                        <a href="#contato" class="home-button">Comprar</a>
+                    </div>
+
+                    <div class="home-image">
+                        <img src="{{ asset('images/img3.png') }}" alt="">
+                    </div>
                 </div>
             </div>
-        </header>
+        </div>
 
-        <main>
+        <div class="container">
 
+            <section class="statistic-counters">
+                <div class="statistic-item">
+                    <h1>1mil</h1>
+                    <p>Clientes atendidos</p>
+                </div>
+                <div class="statistic-item">
+                    <h1>5mil</h1>
+                    <p>Clientes atendidos</p>
+                </div>
+                <div class="statistic-item">
+                    <h1>20uni</h1>
+                    <p>Clientes atendidos</p>
+                </div>
+            </section>
+
+            <section class="about-us">
+                <div class="item-left">
+                    <h1>Sobre Nós</h1>
+                    <h2>Quintal Brinquedos</h2>
+                    <p>Durante a pandemia, percebemos que alguns fornecedores não conseguiam garantir qualidade e segurança nos produtos. Foi então que decidimos tirar do papel um projeto antigo: transformar seu quintal. A Quintal Brinquedos Play atua há 25 anos prestando serviços de manutenção e reforma escolar para escolas da Rede Municipal, Estadual e Prefeituras de São Paulo, com experiência em implantação e manutenção de parques. Seja em escolas, sítios, condomínios ou quintais, contamos com profissionais especializados para garantir excelência em cada projeto.
+
+                    Nossos brinquedos são feitos em Eucalipto Citriodora e Pinus tratados, com durabilidade de 15 anos e pronta entrega. Criamos projetos exclusivos, focados em proporcionar uma experiência única e lúdica para crianças, estimulando a criatividade através do lazer. Somos uma empresa familiar, com o compromisso de melhoria contínua para oferecer experiências consistentes e concretizar sonhos. Vamos transformar seu quintal em um playground divertido? Seja bem-vindo!
+                    </p>
+                </div>
+
+                <div class="item-right">
+                    <img src="{{ asset('images/img1.png') }}" alt="">
+                </div>
+            </section>
+
+        </div>
+
+        <div class="catalog-wrapper">
             <div class="container">
-                <section class="counter">
-                    <div class="counter-item">
-                        <h1>1mil</h1>
-                        <p>Clientes atendidos</p>
+                <section class="catalog">
+                    <div class="catalog-header">
+                        <div class="catalog-titles">
+                            <h1>Catálogo</h1>
+                            <h2>Nossos produtos</h2>
+                        </div>
+
+                        <div class="catalog-links">
+                            <a href="" class="active">Brinquedos</a>
+                            <a href="">Madeiras</a>
+                        </div>
                     </div>
-                    <div class="counter-item">
-                        <h1>5mil</h1>
-                        <p>Clientes atendidos</p>
+
+                    <div class="catalog-body">
+                        <div class="item">
+                            <img src="{{ asset('images/img4.png') }}" alt="">
+                            <h2>Gangorra</h2>
+                            <p>Gangorra com dois assentos, com pneu para amortecimento e a madeira com acabamento em Stain Triplo Filtro Solar.</p>
+                        </div>
+
+                        <div class="item">
+                            <img src="{{ asset('images/img4.png') }}" alt="">
+                            <h2>Gangorra</h2>
+                            <p>Gangorra com dois assentos, com pneu para amortecimento e a madeira com acabamento em Stain Triplo Filtro Solar.</p>
+                        </div>
+
+                        <div class="item">
+                            <img src="{{ asset('images/img4.png') }}" alt="">
+                            <h2>Gangorra</h2>
+                            <p>Gangorra com dois assentos, com pneu para amortecimento e a madeira com acabamento em Stain Triplo Filtro Solar.</p>
+                        </div>
+
+
+                        <div class="item">
+                            <img src="{{ asset('images/img4.png') }}" alt="">
+                            <h2>Gangorra</h2>
+                            <p>Gangorra com dois assentos, com pneu para amortecimento e a madeira com acabamento em Stain Triplo Filtro Solar.</p>
+                        </div>
+
+
+                        <div class="item">
+                            <img src="{{ asset('images/img4.png') }}" alt="">
+                            <h2>Gangorra</h2>
+                            <p>Gangorra com dois assentos, com pneu para amortecimento e a madeira com acabamento em Stain Triplo Filtro Solar.</p>
+                        </div>
+
+
+                        <div class="item">
+                            <img src="{{ asset('images/img4.png') }}" alt="">
+                            <h2>Gangorra</h2>
+                            <p>Gangorra com dois assentos, com pneu para amortecimento e a madeira com acabamento em Stain Triplo Filtro Solar.</p>
+                        </div>
                     </div>
-                    <div class="counter-item">
-                        <h1>20uni</h1>
-                        <p>Clientes atendidos</p>
+
+                    <div class="buttons">
+
                     </div>
                 </section>
-
-
-                <section class="counter">
-                    <div class="counter-item">
-                        <h1>1mil</h1>
-                        <p>Clientes atendidos</p>
-                    </div>
-                    <div class="counter-item">
-                        <h1>5mil</h1>
-                        <p>Clientes atendidos</p>
-                    </div>
-                    <div class="counter-item">
-                        <h1>20uni</h1>
-                        <p>Clientes atendidos</p>
-                    </div>
-                </section>
-
-
-
-
             </div>
+        </div>
 
+        <div class="container">
+            <section class="credibility">
+                <div class="credibility-text">
+                    <h2>Por que Confiar?</h2>
+                    <h3>Quintal Brinquedos</h3>
 
-        </main>
+                    <p>Trabalhamos a anos com todo tipo de prestação de serviços como: construção, reforma, manutenção, adequação de espaços, pisos e casinhas em madeira. Conhecemos as deficiências na prática e no dia a dia de serviços de prestação de serviços, que requer dedicação, confiabilidade, pontualidade, empatia, honestidade e transparência. Bem como fornecemos toda documentação necessária para órgãos públicos. Quem idealiza um projeto e deseja receber durabilidade, beleza, conforto e o mais importante a segurança, escolhe a Quintal Brinquedos.</p>
+                </div>
 
-    </body>
+                <div class="credibility-itens">
+                    <div class="item">
+                        <div class="item-img">
+                            <img src="{{ asset('images/icons/icon-deadline.svg') }}" alt="">
+                        </div>
 
+                        <div class="item-text">
+                            <h3>Entregas no prazo</h3>
+                            <p>Entrega pontual, sem surpresas.</p>
+                        </div>
+
+                    </div>
+
+                    <div class="item">
+
+                        <div class="item-img">
+                            <img src="{{ asset('images/icons/icon-innovators.svg') }}" alt="">
+                        </div>
+
+                        <div class="item-text">
+                            <h3>Brinquedos Inovadores</h3>
+                            <p>Criatividade e diversão em cada detalhe.</p>
+                        </div>
+
+                    </div>
+
+                    <div class="item">
+                        <div class="item-img">
+                            <img src="{{ asset('images/icons/icon-resistant.svg') }}" alt="">
+                        </div>
+
+                        <div class="item-text">
+                            <h3>Brinquedos Resistentes</h3>
+                            <p>Durabilidade que garante diversão por anos.</p>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+        </div>
+
+    </main>
+
+</body>
 </html>
