126641c223bcfef2b63f5245d0dd7345e553c679
Pedro Esteves <pedroesteves2803@gmail.com>
2024-11-15 23:09:32 -0300

    WIP

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'pedrocab498@gmail.com',
+            'name' => 'Admin',
+            'email' => 'jannepatricia@gmail.com',
         ]);
 
         Home::factory()->create();
